b110facda1953251ea9cc7d0499496d5b6b27ce7
Rahmattullah <rfkrhmttllh@gmail.com>
2023-03-14 11:56:26 +0700

    learn conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Starter from '../src/tutorial/02-useEffect/starter/04-fetch-data';
-
+import Starter from '../src/tutorial/03-conditional-rendering/starter/07-user-challenge';
+// import Final from '../src/tutorial/09-context-api/final/prop-drilling';
 function App() {
   return (
     <div className="container">
       <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
